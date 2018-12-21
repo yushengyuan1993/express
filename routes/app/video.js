638e9b3662dfe2ede69ec7a1dd17920b79e3bb6e
yushare <582718643@qq.com>
2018-12-21 15:29:40 +0800

    addd search api

--- routes/app/video.js
+++ routes/app/video.js
@@ -20,4 +20,30 @@ app.post('/get-film', function(req, res, next) {
   res.send(data);
 })
 
+// 搜索
+app.post('/search-film', function(req, res, next) {
+  let title = req.body.title;
+  
+  let subjects = films_all.subjects;
+  let arr = [];
+  for (let i = 0, j = subjects.length; i < j; i ++) {
+    if (subjects[i].title.includes(title)) {
+      arr.push(subjects[i])
+    } else {
+      let casts = subjects[i].casts;
+      let directors = subjects[i].directors;
+
+      let arr2 = casts.concat(directors);
+
+      for (let m = 0; m < arr2.length; m ++) {
+        if (arr2[m].name.includes(title)) {
+          arr.push(subjects[i])
+          break
+        }
+      }
+    }
+  }
+  res.send(arr);
+})
+
 module.exports = app;
