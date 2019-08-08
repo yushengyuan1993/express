6685f03cd19c1cdfc54ce2ecef5976720ec0859f
yushare <582718643@qq.com>
2019-08-08 17:25:08 +0800

    update

--- routes/app/keep.js
+++ routes/app/keep.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.post('/get-keep', function(req, res, next) {
   res.send({
     video_id: '5285890790782139610',
-    video_src: 'http://1254368367.vod2.myqcloud.com/78d1c9ccvodgzp1254368367/ee77c7cf5285890790782139610/54JWtB6aah0A.mp4'
+    video_src: 'http://1254368367.vod2.myqcloud.com/2cf96c74vodtransgzp1254368367/f7e1a4ec5285890784532365214/v.f40.mp4'
   })
 })
 
