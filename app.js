e0fcb7237f0c51e1fb86d5b9a100985d936118f2
yushare <582718643@qq.com>
2019-10-31 10:47:33 +0800

    update

--- app.js
+++ app.js
@@ -23,14 +23,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 设置跨入请求
-// app.all('*', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
+// 设置跨域请求
+const allowCors = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+};
+app.use(allowCors);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/api/activity.js
+++ routes/api/activity.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/double-eleven', (req, res, next) => {
+router.post('/v-1111', (req, res, next) => {
   res.send({
     code: 0,
     data: {
-      name: 'ysy'
+      remaining_time: 2342167,
+      banner_img: "banner_1",
+      border_img: "border_1",
+      tag_img: "tag_1",
+      is_activity_product: 1
     },
     message: 'success',
     timestamp: + new Date()
